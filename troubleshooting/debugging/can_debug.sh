c200b495c92d7e57968229cac1795b931d879f14
Esoterical <124253477+Esoterical@users.noreply.github.com>
2024-02-14 18:46:09 +1000

    Update can_debug.sh

--- troubleshooting/debugging/can_debug.sh
+++ troubleshooting/debugging/can_debug.sh
@@ -1,42 +1,85 @@
 #!/bin/sh
 
 #Credit to @bolliostu for the original idea and script
-#Credit to @dormausian for the disclaimer and temperauture sections
+#Credit to @dormouse for a whole lot, including making the script not look like hot garbage
 
-echo "*************"
-echo "* Attention *"
-echo "*************"
-echo
-echo "This script will run a series of diagnostic commands to gather configuration"
-echo "information about this host and upload it to a public site where it may be"
-echo "viewed by others. It will contain no personal information."
-echo "It may also be necessary to install an additional package"
-echo "to help facilitate this task."
-echo "You can view this script at:"
-echo "https://github.com/Esoterical/voron_canbus/blob/main/troubleshooting/debugging/can_debug.sh"
-echo
+# Directories and files
+KATAPULTDIR="$HOME/katapult"
+CANBOOTDIR="$HOME/CanBoot"
+KLIPPERDIR="$HOME/klipper"
+KLIPPYLOG="$HOME/printer_data/logs/klippy.log"
 
-while true; do
-    read -p "Do you wish to run this program? (yes/no) " yn < /dev/tty
-    case $yn in
-        [Yy]* ) break;;
-        [Nn]* ) exit;;
-        * ) echo "Please answer yes or no.";;
-    esac
-done
+# Default responses 
+CANSTATS="No can0 interface"
+CANQUERY="No can0 interface"    
+BYID="/dev/serial/by-id Not Found"
+CAN0IFACE="/etc/network/interfaces.d/can0 Not Found"
 
-echo "Confirmed script execution"
+BOOTLOADERDIRFND="Not Found"
+BOOTLDRFND="Not Found"
+KLIPPERDIRFND="Not Found"
+KLIPPERFND="Not Found"
+KLIPPERVER="Klipper not installed"
+
+MCUINFOFND="Not Found"
+PRNTDATAFND="Not Found"
+KLIPPERCFG="Not Found"
+ADC="Data Unavailable"
+
+disclaimer() {
+	echo "*************"
+	echo "* Attention *"
+	echo "*************"
+	echo
+	echo "This script will run a series of diagnostic commands to gather configuration"
+	echo "information about this host and upload it to a public site where it may be"
+	echo "viewed by others. It will contain no personal information."
+	echo "It may also be necessary to install an additional package to help facilitate"
+	echo "this task.  This script is available for review at: "
+	echo "https://github.com/Esoterical/voron_canbus/blob/main/troubleshooting/debugging/can_debug.sh"
+	echo
+
+	while true; do
+		read -p "Do you wish to run this program? (yes/no) " yn < /dev/tty
+		case $yn in
+			[Yy]* ) break;;
+			[Nn]* ) exit;;
+			* ) echo "Please answer yes or no.";;
+		esac
+	done
+}
+
+checknc() {
+	# Checks for nc command and installs modern version of netcat on Debian based systems if not found.
+	if ! command -v nc > /dev/null 2>&1 ; then
+		echo "NetCat not found on system"
+		echo "Installing Netcat with 'sudo apt-get install netcat-openbsd -qq > /dev/null'"
+		sudo apt-get install netcat-openbsd -qq > /dev/null
+	fi
+}
 
 
-# Checks for nc command and installs modern version of netcat on Debian based systems if not found.
-if ! command -v nc > /dev/null 2>&1 ; then
-    echo "NetCat not found on system"
-    echo "Installing Netcat with 'sudo apt-get install netcat-openbsd -qq > /dev/null'"
-    sudo apt-get install netcat-openbsd -qq > /dev/null
-fi
+# Formatting fuction for output sections
+# Usage: prepout <HEADER> [SUBSECTION...]
 
-# Definition of commands to be be run to obtain relavent information regarding CAN bus configuration.
-PRETTY_LINE_BRK="================================================================"
+prepout() {
+	echo
+	echo "================================================================";
+	echo $1;
+	echo "================================================================\n";
+	# shift the first array entry $1 (Header) and iterate through remaining 
+	shift
+	for var in "$@"; do echo "$var\n"; done
+}
+
+#######################################
+
+disclaimer;
+checknc;
+
+echo "\nGathering Data...\n"
+
+# Definition of commands to be be run to obtain relevant information regarding CAN bus configuration.
 MODEL="$(cat /sys/firmware/devicetree/base/model)"
 DISTRO="$(cat /etc/*-release)"
 KERNEL="$(uname -a)"
@@ -45,26 +88,17 @@ IFACESERVICE="$(ls /etc/network)"
 IPA="$(ip a)"
 
 if ip a | grep -q -E "can0"; then
-    CANSTATS="$(ip -d -s link show can0)"
-    CANQUERY="$(~/klippy-env/bin/python ~/klipper/scripts/canbus_query.py can0)"
-else
-    CANSTATS="No can0 interface"
-    CANQUERY="No can0 interface"    
+	CANSTATS="$(ip -d -s link show can0)"
+	CANQUERY="$(~/klippy-env/bin/python ~/klipper/scripts/canbus_query.py can0)"
 fi
 
 LSUSB="$(lsusb)"
 
 if [ -d /dev/serial/by-id ]; then
-    BYID="$(ls -l /dev/serial/by-id | awk -F' ' '{print $9,$10,$11}')"
-else
-    BYID="No /dev/serial/by-id found"
+	BYID="$(ls -l /dev/serial/by-id | awk '{print $9,$10,$11}')"
 fi
 
 # BITVERSION="$(getconf LONG_BIT)"
-
-
-
-
 
 # List of systemd filess.
 SYSTEMD="$(ls /etc/systemd/network)"
@@ -72,132 +106,80 @@ SYSTEMD="$(ls /etc/systemd/network)"
 # Contents of rc.local
 RCLOCAL="$(cat /etc/rc.local)"
 
-# Identification of directories pertainent to CAN fw compilation files.
-KATAPULTDIR="/home/$USER/katapult/"
-CANBOOTDIR="/home/$USER/CanBoot/"
-CANFND="NOT Found"
-
-
-
-KLIPPERDIR="/home/$USER/klipper/"
-KLIPPERFND="NOT Found"
-
-PRNTDATA="/home/$USER/printer_data/logs"
-PRNTDATAFND="NOT Found"
-#grep "MCU 'mcu' config" ~/printer_data/logs/klippy.log | tail -1
+#grep "MCU 'mcu' config" $KLIPPYLOG | tail -1
 
 # Checking Linux Network configuration.
 if [ -f /etc/network/interfaces.d/can0 ]; then
-    NETWORK=$(cat /etc/network/interfaces.d/can0)
-else
-    NETWORK="can0 network not found in /etc/network/interfaces.d/"
+	CAN0IFACE=$(cat /etc/network/interfaces.d/can0)
 fi
 
-# Retrieving bootloader compilation configuration.
+# Retrieving katapult bootloader compilation configuration.
 if [ -d ${KATAPULTDIR} ]; then
-    if [ -f ${KATAPULTDIR}/.config ]; then
-        CANFND="Found\n\nKatapult Config:\n$(cat ${KATAPULTDIR}/.config)"
-    else
-        CANFND="Found\n\nKatapult Make Config: Not Found"
-    fi
+    BOOTLOADERDIRFND="Found";
+	if [ -f ${KATAPULTDIR}/.config ]; then
+		BOOTLOADERFND="\n$(cat ${KATAPULTDIR}/.config)"
+	fi
+# Retrieving CanBoot bootloader compilation configuration if katapult not there.
 elif [ -d ${CANBOOTDIR} ]; then
-    if [ -f ${CANBOOTDIR}/.config ]; then
-        CANFND="Found\n\nCanBoot Config:\n$(cat ${CANBOOTDIR}/.config)"
-    else
-        CANFND="Found\n\nCanBoot Make Config: Not Found"
-    fi
-fi
+	BOOTLOADERDIRFND="Found"
+	if [ -f ${CANBOOTDIR}/.config ]; then
+		BOOTLOADERFND="\n$(cat ${CANBOOTDIR}/.config)"
+	fi
+fi;
 
 # Retrieving klipper firmware compilation configuration.
 if [ -d ${KLIPPERDIR} ]; then
-    if [ -f ${KLIPPERDIR}/.config ]; then
-        KLIPPERFND="Found\n\nKlipper Config:\n$(cat ${KLIPPERDIR}/.config)"
-        if command -v git > /dev/null 2>&1; then
+    KLIPPERDIRFND="Found";
+	if [ -f ${KLIPPERDIR}/.config ]; then
+		KLIPPERFND="\n$(cat ${KLIPPERDIR}/.config)"
+		if command -v git > /dev/null 2>&1; then
             cd ~/klipper
             KLIPPERVER="$(git describe --tags)"
         fi
-    else
-        KLIPPERFND="Found\n\nKlipper Make Config: Not Found"
-        KLIPPERVER="Klipper not installed"
-    fi
+	fi
 fi
-
-
 
 # Retrieving info from klippy.log
-if [ -d ${PRNTDATA} ]; then
-    if [ -f ${PRNTDATA}/klippy.log ]; then
-        PRNTDATAFND="Found\n\nKlippy Log:\n$(grep "MCU 'mcu' config" ~/printer_data/logs/klippy.log | tail -1)"
-        KLIPPERCFG="$(tac ~/printer_data/logs/klippy.log | awk '/=======================/&&++k==1,/===== Config file =====/' | tac)"
+if [ -f $KLIPPYLOG ]; then
+	MCUINFOFND="Found"
+	PRNTDATAFND="$(grep "MCU 'mcu' config" $KLIPPYLOG | tail -1)"
+	KLIPPERCFG="$(tac $KLIPPYLOG | awk '/=======================/&&++k==1,/===== Config file =====/' | tac)"
        
-        # ADC temp check
-        MIN_TEMP=-10
-        MAX_TEMP=400
-        ADC=$(tac ~/printer_data/logs/klippy.log | grep -m 1 "^Stats" | sed 's/\([a-zA-Z0-9_.]*\)\:/\n\1:/g' |
-                awk -v mintemp="$MIN_TEMP" -v maxtemp="$MAX_TEMP" '/temp=/ {
-                        printf "%18s ", $1;
-                        for (i=2; i<=split($0, stat, " "); i++) {
-                                if (sub(/temp=/, "", stat[i])) {
-                                        printf "%6s", stat[i];
-                                        if (stat[i] + 0 < mintemp ) {
-                                                printf "%s", "    *** Check Sensor ***";
-                                        } else if (stat[i] + 0 > maxtemp) {
-                                                printf "%s", "    *** Check Sensor ***";
-                                        }
-                                        break;
-                                }
-                        }
-                        printf "\n";
-                }')
-    else
-        PRNTDATAFND="Found\n\nKlippy Log: Not Found"
-        KLIPPERCFG="Found\n\nKlippy Log: Not Found"
-        ADC="Found\n\nKlippy Log: Not Found"
-    fi
+	# ADC temp check
+	MIN_TEMP=-10
+	MAX_TEMP=400
+	ADC=$(tac $KLIPPYLOG | grep -m 1 "^Stats" | sed 's/\([a-zA-Z0-9_.]*\)\:/\n\1:/g' |
+		awk -v mintemp="$MIN_TEMP" -v maxtemp="$MAX_TEMP" '/temp=/ {
+			printf "%18s ", $1;
+			for (i=2; i<=split($0, stat, " "); i++) {
+				#printf "%s", stat;
+				if (sub(/temp=/, "", stat[i])) {
+					printf "%6s", stat[i];
+					if (stat[i] + 0 < mintemp ) {
+						printf "%s", "    *** Check Sensor ***";
+					} else if (stat[i] + 0 > maxtemp) {
+						printf "%s", "    *** Check Sensor ***";
+					}
+					break;
+				}
+			}
+			printf "\n";
+		}'
+	)
 fi
-# if [ -d ${PRNTDATA} ]; then
-#     if [ -f ${PRNTDATA}/klippy.log ]; then
-#         KLIPPY_LOG=$HOME/printer_data/logs/klippy.log
-#         #KLIPPY_LOG=$HOME/dev/klippytemp/new.log
-#         MIN_TEMP=-10
-#         MAX_TEMP=400
-        
-#         TEMPERATURECHECK=$(tac ~/printer_data/logs/klippy.log | grep -m 1 "^Stats" | sed 's/\([a-zA-Z0-9_.]*\)\:/\n\1:/g' |
-#                 awk -v mintemp="$MIN_TEMP" -v maxtemp="$MAX_TEMP" '/temp=/ {
-#                         printf "%18s ", $1;
-#                         for (i=2; i<=split($0, stat, " "); i++) {
-#                                 if (sub(/temp=/, "", stat[i])) {
-#                                         printf "%6s", stat[i];
-#                                         if (stat[i] + 0 < mintemp ) {
-#                                                 printf "%s", "*** Check Sensor ***";
-#                                         } else if (stat[i] + 0 > maxtemp) {
-#                                                 printf "%s", "*** Check Sensor ***";
-#                                         }
-#                                         break;
-#                                 }
-#                         }
-#                         printf "\n";
-#                 }')
-#     fi
-# fi
-# Formatting outpur
-#TXT_OS="${PRETTY_LINE_BRK}\nOS\n${PRETTY_LINE_BRK}\n\nDistro:\n${DISTRO}\n\nKernel:\n${KERNEL}\n\nBits:\n${BITVERSION}"
-TXT_OS="${PRETTY_LINE_BRK}\nOS\n${PRETTY_LINE_BRK}\n\nModel:\n${MODEL}\n\nDistro:\n${DISTRO}\n\nKernel:\n${KERNEL}\n\nKlipper Version:\n${KLIPPERVER}\n\nUptime:\n${UPTIME}"
-TXT_NET="\n\n${PRETTY_LINE_BRK}\nNetwork\n${PRETTY_LINE_BRK}\n\nInterface Services:\n${IFACESERVICE}\n\ncan0:\n${NETWORK}\n\nip a:\n${IPA}\n\ncan0 ifstats:\n${CANSTATS}" 
-TXT_SYSD="\n\n${PRETTY_LINE_BRK}\nSystemd Network Files\n${PRETTY_LINE_BRK}\n\n${SYSTEMD}"
-TXT_RCL="\n\n${PRETTY_LINE_BRK}\nrc.local contents\n${PRETTY_LINE_BRK}\n\n${RCLOCAL}"
-TXT_USB="\n\n${PRETTY_LINE_BRK}\nUSB\n${PRETTY_LINE_BRK}\n\nlsusb:\n${LSUSB}"
-TXT_BYID="\n\n${PRETTY_LINE_BRK}\nDev Serial By-ID\n${PRETTY_LINE_BRK}\n\nDev Serial By-ID:\n${BYID}"
-TXT_CANQ="\n\n${PRETTY_LINE_BRK}\nCANQuery\n${PRETTY_LINE_BRK}\n\nCANBus Query:\n${CANQUERY}"
-TXT_LOG="\n\n${PRETTY_LINE_BRK}\nMCU\n${PRETTY_LINE_BRK}\n\nMCUInfo:\n${PRNTDATAFND}"
-TXT_ADC="\n\n${PRETTY_LINE_BRK}\nTemperature Check\n${PRETTY_LINE_BRK}\n\n${ADC}"
-TXT_CAN="\n\n${PRETTY_LINE_BRK}\nKatapult\n${PRETTY_LINE_BRK}\n\nKatapult Directory: ${CANFND}"
-TXT_KLP="\n\n${PRETTY_LINE_BRK}\nKlipper\n${PRETTY_LINE_BRK}\n\nKlipper Directory: ${KLIPPERFND}"
-TXT_CFG="\n\n${PRETTY_LINE_BRK}\nKlipperConfig\n${PRETTY_LINE_BRK}\n\n${KLIPPERCFG}"
-
-
-# Reminding user of nice place to get help and letting them know the termbin link.
-echo "The following link will have your information:"
 
 # Sending to termbin and obtaining link.
-echo "${TXT_OS} ${TXT_NET} ${TXT_SYSD} ${TXT_RCL} ${TXT_USB} ${TXT_BYID} ${TXT_CANQ} ${TXT_LOG} ${TXT_ADC} ${TXT_CAN} ${TXT_KLP} ${TXT_CFG}" | nc termbin.com 9999
+echo "Uploading...\n"
+echo "$(prepout "OS" "Model:\n${MODEL}" "Distro:\n${DISTRO}" "Kernel:\n${KERNEL}" "Klipper Version:\n${KLIPPERVER}" "Uptime:\n${UPTIME}") 
+$(prepout "Network" "Interface Services:\n${IFACESERVICE}" "can0:\n${CAN0IFACE}" "ip a:\n${IPA}" "can0 ifstats:\n${CANSTATS}")
+$(prepout "Systemd Network Files" "${SYSTEMD}")
+$(prepout "rc.local contents" "${RCLOCAL}")
+$(prepout "USB" "lsusb:\n${LSUSB}")
+$(prepout "Dev Serial By-ID" "Dev Serial By-ID:\n${BYID}")
+$(prepout "CANQuery" "CANBus Query:\n${CANQUERY}")
+$(prepout "MCU" "MCUInfo:\n${MCUINFOFND}" "Klippy Log:\n${PRNTDATAFND}")
+$(prepout "Temperature Check" "${ADC}")
+$(prepout "Bootloader" "Bootloader Directory: ${BOOTLOADERDIRFND}" "Bootloader Make Config: ${BOOTLOADERFND}")
+$(prepout "Klipper" "Klipper Directory: ${KLIPPERDIRFND}" "Klipper Make Config: $KLIPPERFND")
+$(prepout "KlipperConfig" "${KLIPPERCFG}")" |
+	nc termbin.com 9999 | { read url; echo "Information available at the following URL:"; echo "$url"; }
